a06422a75dce0e96a46d774980e49ccde3022c4e
Piotr Trybała <trybalapiotr4@gmail.com>
2023-02-09 20:42:49 +0100

    [Main] added engine test for correct compound

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,26 @@ pub mod tests {
     fn test_base_has_correct_compounds() {
         let engine = DecoderEngine::new("./schema/schema.json".to_string());
 
+        println!("{}", engine.base_size());
+
         assert!(engine.base_size() == 26);
+    }
+
+    #[test]
+
+    fn test_correct_compound_selected() {
+        let engine = DecoderEngine::new("./schema/schema.json".to_string());
+        assert_eq!(engine.get_symbol("AUG".to_string()), "M_");
+        assert_eq!(engine.get_symbol("GGG".to_string()), "G");
+        assert_eq!(engine.get_symbol("GGU".to_string()), "G");
+        assert_eq!(engine.get_symbol("GUA".to_string()), "V");
+        assert_eq!(engine.get_symbol("GUG".to_string()), "V");
+        assert_eq!(engine.get_symbol("UAA".to_string()), "*");
+        assert_eq!(engine.get_symbol("UAG".to_string()), "*");
+        assert_eq!(engine.get_symbol("CCC".to_string()), "P");
+        assert_eq!(engine.get_symbol("CAU".to_string()), "H");
+        assert_eq!(engine.get_symbol("AGG".to_string()), "R");
+        assert_eq!(engine.get_symbol("UUU".to_string()), "F");
     }
 
 }
