7a8143cdaab844b25967f4a37be396e112933711
Piotr Trybała <trybalapiotr4@gmail.com>
2023-02-09 16:41:30 +0100

    [Engine] defined engine types

---
+++ src/engine.rs
@@ -0,0 +1,15 @@
+
+pub struct DecoderEngine {
+    compound_base: CompoundBase
+}
+
+pub struct Compound {
+    symbol: String,
+    code: String
+}
+
+pub struct CompoundBase {
+    compounds: Vec<Compound>
+}
+
+
