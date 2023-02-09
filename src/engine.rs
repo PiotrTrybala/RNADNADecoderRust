faeebb469c9b8fbaa3ab7c441d123d2e2af8e6da
Piotr Trybała <trybalapiotr4@gmail.com>
2023-02-09 16:46:07 +0100

    [Engine] created methods and constructors for DecoderEngine and CompoundBase

--- src/engine.rs
+++ src/engine.rs
@@ -9,7 +9,41 @@ pub struct Compound {
 }
 
 pub struct CompoundBase {
+    schema_file: String,
     compounds: Vec<Compound>
+}
+
+impl DecoderEngine {
+
+    fn new(schema_file: String) -> Self {
+
+        let base = CompoundBase::new(schema_file);
+
+        Self {
+            compound_base: base
+        }
+    }
+
+}
+
+impl CompoundBase {
+
+    fn new(schema_file: String) -> Self {
+
+        let compounds = CompoundBase::gen_compound_base(&schema_file);
+
+        Self {
+            schema_file,
+            compounds
+        }
+    }
+
+    fn gen_compound_base(schema_file: &String) -> Vec<Compound> {
+
+        Vec::<Compound>::new()
+
+    }
+
 }
 
 
