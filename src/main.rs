f085007e885fdbc71086ab5a4d7e48ea9017760c
Piotr Trybała <trybalapiotr4@gmail.com>
2023-02-09 17:01:10 +0100

    [Test] added test for path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+pub mod engine;
+
+use engine::DecoderEngine;
+
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    
+
+
+    let decoderEngine = DecoderEngine::new("/schema/schema.json".to_string());
+
+
+}
+#[cfg(test)]
+#[test]
+fn path_test() {
+    let cwd = env::current_dir().unwrap();
+
+    println!("Current working directory: {}", cwd.display());
+
+    let path = Path::new("./schema/schema.json");
+    assert_eq!(path.exists(), true);
 }
